19ced793afc1c7c7b72df9bf7ff2b70e6247156f
ervnjmsdnts <earvinjamesdantes@gmail.com>
2021-11-03 02:44:10 +0800

    added handling refresh token feature

--- server/src/server.js
+++ server/src/server.js
@@ -2,12 +2,40 @@ import "dotenv/config";
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import { verify } from "jsonwebtoken";
 
 import typeDefs from "./graphql/typeDefs";
 import resolvers from "./graphql/resolvers";
+import { User } from "./models/user";
+import { createAccessToken, createRefreshToken } from "./utils/token";
 
 (async () => {
   const app = express();
+
+  //Handling Refresh Token
+  app.use(cookieParser());
+  app.post("/refresh_token", (req, res) => {
+    const token = req.cookies.jai;
+
+    if (!token) {
+      return res.send({ accessToken: "" });
+    }
+
+    let payload = null;
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET);
+    } catch (error) {
+      console.log(error);
+      return res.send({ accessToken: "" });
+    }
+
+    const user = User.findOne({ id: payload.userId });
+
+    res.cookie("jai", createRefreshToken(user), { httpOnly: true });
+
+    return res.send({ accessToken: createAccessToken(user) });
+  });
 
   const server = new ApolloServer({
     typeDefs,
