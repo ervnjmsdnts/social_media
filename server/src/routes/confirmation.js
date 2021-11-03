a07dd9f53016c6a0c90ab47ca2f79d59e85cfc48
ervnjmsdnts <earvinjamesdantes@gmail.com>
2021-11-03 20:13:48 +0800

    Added email confirmation feature

---
+++ server/src/routes/confirmation.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { User } from "../models/user";
+import { verify } from "jsonwebtoken";
+
+const router = express.Router();
+
+router.get("/:token", async (req, res) => {
+  try {
+    const { userId } = verify(req.params.token, process.env.EMAIL_TOKEN_SECRET);
+    const user = await User.findById(userId);
+    await user.updateOne({ confirmed: true });
+  } catch (error) {
+    console.log(error);
+    return res.send({ emailConfirmed: "" });
+  }
+
+  return res.send({ emailConfirmed: "confirmed" });
+});
+
+export default router;
