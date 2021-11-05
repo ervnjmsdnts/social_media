19487c04a498b840b747d7429beb61ef21ce2fc7
ervnjmsdnts <earvinjamesdantes@gmail.com>
2021-11-06 05:13:58 +0800

    encapsulated getToken logic with a try catch

--- client/src/pages/Confirmation.jsx
+++ client/src/pages/Confirmation.jsx
@@ -11,10 +11,14 @@ const Confirmation = () => {
 
   useEffect(() => {
     const getToken = async () => {
-      const response = await axios.get(
-        `http://localhost:5000/confirmation/${token}`
-      );
-      setIsValidToken(response.data);
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/confirmation/${token}`
+        );
+        setIsValidToken(response.data);
+      } catch (error) {
+        console.log(error);
+      }
     };
     getToken();
   }, [token]);
@@ -26,7 +30,9 @@ const Confirmation = () => {
   return (
     <div className="flex justify-center items-center flex-col h-screen bg-primary">
       <h1 className="text-secondary text-4xl text-center md:text-7xl font-semibold">
-        Your Email Address has been Confirmed
+        {isValidToken
+          ? "Your Email Address has been Confirmed"
+          : "Email Address is Invalid"}
       </h1>
       <Button
         className="text-primary bg-secondary mt-8 text-3xl w-[300px]"
