a268547a0cf9ff828240a9b428a6cc35df81773e
ervnjmsdnts <earvinjamesdantes@gmail.com>
2021-11-06 05:03:30 +0800

    Added redirect on confirmation page to auth page

--- client/src/pages/Confirmation.jsx
+++ client/src/pages/Confirmation.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router-dom";
+
+import Button from "../components/Button";
 
 const Confirmation = () => {
   const { token } = useParams();
+  const navigate = useNavigate();
   const [isValidToken, setIsValidToken] = useState(true);
 
   useEffect(() => {
@@ -16,7 +19,22 @@ const Confirmation = () => {
     getToken();
   }, [token]);
 
-  return <div>{isValidToken ? "Email is confirmed" : "Email is invalid"}</div>;
+  const handleClick = () => {
+    navigate("/auth", { replace: true });
+  };
+
+  return (
+    <div className="flex justify-center items-center flex-col h-screen bg-primary">
+      <h1 className="text-secondary text-4xl text-center md:text-7xl font-semibold">
+        Your Email Address has been Confirmed
+      </h1>
+      <Button
+        className="text-primary bg-secondary mt-8 text-3xl w-[300px]"
+        onClick={handleClick}>
+        Go Back
+      </Button>
+    </div>
+  );
 };
 
 export default Confirmation;
